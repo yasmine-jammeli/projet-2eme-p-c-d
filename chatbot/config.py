77ece2b460753fe5c3340a256c7a590d09a9ebb9
yasmine-jammeli <120276958+yasmine-jammeli@users.noreply.github.com>
2024-04-23 22:36:56 +0100

    0
    0

--- chatbot/config.py
+++
@@ -1,8 +0,0 @@
-import os
-class Config:
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 465
-    MAIL_USE_TLS = False
-    MAIL_USE_SSL = True
-    MAIL_USERNAME = 'your_gmail_adress_here'
-    MAIL_PASSWORD = 'your_gmail_app_password_here'
